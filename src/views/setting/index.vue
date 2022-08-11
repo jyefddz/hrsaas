6afb8d49ecdfa353c3da4d263e928fb3f6c5b304
jyefddz <1065697146@qq.com>
2022-08-11 14:03:53 +0800

    完成公司设置,公司权限未做

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -30,41 +30,67 @@
           >
           </el-pagination>
         </el-tab-pane>
-        <el-tab-pane label="公司信息" name="second">公司信息</el-tab-pane>
+        <!-- 弹层 -->
+        <el-dialog
+          @close="dialogClose"
+          title="新增角色"
+          :visible.sync="addDialogVisible"
+          width="50%"
+        >
+          <!-- :model -->
+          <!-- v-model @input :value -->
+          <el-form
+            ref="form"
+            label-width="80px"
+            :model="addRoleForm"
+            :rules="addRoleFormRules"
+          >
+            <el-form-item label="角色名称" prop="name">
+              <el-input v-model="addRoleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="角色描述">
+              <el-input v-model="addRoleForm.description"></el-input>
+            </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="onClose">取 消</el-button>
+            <el-button type="primary" @click="onAddRole">确 定</el-button>
+          </span>
+        </el-dialog>
+        <el-tab-pane label="公司信息" name="second">
+          <el-alert
+            title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+            type="info"
+            show-icon
+            :closable="false"
+          >
+          </el-alert>
+          <el-form ref="form" label-width="80px">
+            <el-form-item label="公司名称">
+              <el-input v-model="companyInfo.name" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="公司地址">
+              <el-input
+                v-model="companyInfo.companyAddress"
+                disabled
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="公司邮箱">
+              <el-input v-model="companyInfo.mailbox" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="备注">
+              <el-input v-model="companyInfo.remarks" disabled></el-input>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
       </el-tabs>
     </div>
-    <!-- 弹层 -->
-    <el-dialog
-      @close="dialogClose"
-      title="新增角色"
-      :visible.sync="addDialogVisible"
-      width="50%"
-    >
-      <!-- :model -->
-      <!-- v-model @input :value -->
-      <el-form
-        ref="form"
-        label-width="80px"
-        :model="addRoleForm"
-        :rules="addRoleFormRules"
-      >
-        <el-form-item label="角色名称" prop="name">
-          <el-input v-model="addRoleForm.name"></el-input>
-        </el-form-item>
-        <el-form-item label="角色描述">
-          <el-input v-model="addRoleForm.description"></el-input>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="onClose">取 消</el-button>
-        <el-button type="primary" @click="onAddRole">确 定</el-button>
-      </span>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import { getRolesApi, addRolesApi } from '@/api/role'
+import { getCompanyInfoApi } from '@/api/setting'
 export default {
   data() {
     return {
@@ -83,12 +109,14 @@ export default {
             trigger: 'blur'
           }
         ]
-      }
+      },
+      companyInfo: { name: '', companyAddress: '', mailbox: '', remarks: '' }
     }
   },
 
   created() {
     this.getRoles()
+    this.getCompanyInfo()
   },
 
   methods: {
@@ -123,6 +151,13 @@ export default {
     dialogClose() {
       this.$refs.form.resetFields()
       this.addRoleForm.description = ''
+    },
+    async getCompanyInfo() {
+      const res = await getCompanyInfoApi(
+        this.$store.state.user.userInfo.companyId
+      )
+      this.companyInfo = res
+      console.log(this.companyInfo)
     }
   }
 }
